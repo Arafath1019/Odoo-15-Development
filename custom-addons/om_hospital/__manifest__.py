613b8a2afc13529f192a0cf08c5b2f4e3e5b3615
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2023-12-26 00:54:22 +0600

    feat: create hospital management system module

---
+++ custom-addons/om_hospital/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'sequence': 1,
+    'author': 'Yeasin Arafath',
+    'summary': 'Hospital Management System',
+    'description': """Hospital Management System""",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
