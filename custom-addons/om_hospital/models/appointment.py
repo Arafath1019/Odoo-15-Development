a41262fe1c33d2c3eb862537cceac62fba47fc6f
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2023-12-31 22:03:52 +0600

    feat: add date and datetime field

--- custom-addons/om_hospital/models/appointment.py
+++ custom-addons/om_hospital/models/appointment.py
@@ -6,4 +6,6 @@ class HospitalAppointment(models.Model):
     _description = "Hospital Appointment"
     
     patient_id = fields.Many2one(comodel_name="hospital.patient", string="Appointment")
+    appointment_time = fields.Datetime(string="Appointment Time")
+    booking_date = fields.Date(string="Booking Date")
     
