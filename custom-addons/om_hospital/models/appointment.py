ebf09eb3d0ce6d8ae14628727a47f80ded41be20
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2023-12-31 22:10:32 +0600

    feat: set default values for fields

--- custom-addons/om_hospital/models/appointment.py
+++ custom-addons/om_hospital/models/appointment.py
@@ -6,6 +6,6 @@ class HospitalAppointment(models.Model):
     _description = "Hospital Appointment"
     
     patient_id = fields.Many2one(comodel_name="hospital.patient", string="Appointment")
-    appointment_time = fields.Datetime(string="Appointment Time")
-    booking_date = fields.Date(string="Booking Date")
+    appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
+    booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
     
--- custom-addons/om_hospital/models/patient.py
+++ custom-addons/om_hospital/models/patient.py
@@ -6,7 +6,7 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient"
     
     name = fields.Char(string="Name", tracking=True)
-    ref = fields.Char(string="Reference")
+    ref = fields.Char(string="Reference", default="Odoo 15 Development")
     age = fields.Integer(string="Age", tracking=True)
-    gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender", tracking=True)
+    gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender", tracking=True, default="female")
     active = fields.Boolean(string="Active", default=True)
