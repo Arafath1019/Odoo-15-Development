d43d22ada5e5068cd7f32530784c76eaeadac878
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2023-12-31 17:52:34 +0600

    feat: apply domain in menu

--- custom-addons/om_hospital/__manifest__.py
+++ custom-addons/om_hospital/__manifest__.py
@@ -10,7 +10,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
-        'views/patient_view.xml'
+        'views/patient_view.xml',
+        'views/female_patient_view.xml'
     ],
     'demo': [],
     'application': True,
