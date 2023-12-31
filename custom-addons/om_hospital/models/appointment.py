8cb1cab2c926f6497ecef9ac1c003ee205a11724
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2023-12-31 22:28:31 +0600

    feat: add related fields

--- custom-addons/om_hospital/models/appointment.py
+++ custom-addons/om_hospital/models/appointment.py
@@ -8,4 +8,6 @@ class HospitalAppointment(models.Model):
     patient_id = fields.Many2one(comodel_name="hospital.patient", string="Appointment")
     appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
     booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
+    # gender = fields.Selection(related="patient_id.gender", readonly=False)
+    gender = fields.Selection(related="patient_id.gender")
     
