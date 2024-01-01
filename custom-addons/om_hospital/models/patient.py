ae6953444d893d69024b24537af49e7488bf3a6a
yeasin.arafath <yeasin.arafath@bjitgroup.com>
2024-01-01 10:58:31 +0600

    feat: create compute field

--- custom-addons/om_hospital/models/patient.py
+++ custom-addons/om_hospital/models/patient.py
@@ -1,4 +1,5 @@
 from odoo import api, models, fields
+from datetime import date
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
@@ -7,6 +8,16 @@ class HospitalPatient(models.Model):
     
     name = fields.Char(string="Name", tracking=True)
     ref = fields.Char(string="Reference", default="Odoo 15 Development")
-    age = fields.Integer(string="Age", tracking=True)
+    date_of_birth = fields.Date(string="Date of birth")
+    age = fields.Integer(string="Age", compute="_compute_age", tracking=True)
     gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender", tracking=True, default="female")
     active = fields.Boolean(string="Active", default=True)
+    
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 0
