f5e2ec60a0cef373fe29ef4f7edfd4a7a7256919
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2024-01-07 17:16:50 +0600

    feat: widget boolean toggle

---
+++ custom-addons/om_hospital/models/patient_tag.py
@@ -0,0 +1,8 @@
+from odoo import api, models, fields
+
+class PatientTag(models.Model):
+    _name = "patient.tag"
+    _description = "Patient Tag"
+    
+    name = fields.Char(string="Name", required=True)
+    active = fields.Boolean(string="Active", default=True)
