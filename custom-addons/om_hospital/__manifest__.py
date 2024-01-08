7cbce80aca48cf4bc361e48659a352415286a8b1
yeasin.arafath <yeasin.arafath@bjitgroup.com>
2024-01-08 17:24:46 +0600

    feat: create and launch wizard

--- custom-addons/om_hospital/__manifest__.py
+++ custom-addons/om_hospital/__manifest__.py
@@ -9,11 +9,12 @@
     'depends': ['mail', 'product'],
     'data': [
         'security/ir.model.access.csv',
+        'wizard/cancel_appointment_view.xml',
         'views/menu.xml',
         'views/patient_view.xml',
         'views/female_patient_view.xml',
         'views/appointment_view.xml',
-        'views/patient_tag_view.xml'
+        'views/patient_tag_view.xml',
     ],
     'demo': [],
     'application': True,
