3c1edde20e1f7c53d35d44a593eaab34176d5063
yeasin.arafath <yeasin.arafath@bjitgroup.com>
2024-01-11 16:42:17 +0600

    feat: inherit model

---
+++ custom-addons/om_odoo_inheritance/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Odoo Inheritance',
+    'version': '1.0.0',
+    'category': 'Inhertance',
+    'sequence': 1,
+    'author': 'Yeasin Arafath',
+    'summary': 'Odoo Inheritance',
+    'description': """Odoo Inheritance""",
+    'depends': ['sale'],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
