dcb5e4333ac45168ec43738b5cefd6fa55db5590
yeasin.arafath <yeasin.arafath@bjitgroup.com>
2024-02-02 11:52:28 +0600

    feat: ondelete policy

--- custom-addons/om_hospital/models/appointment.py
+++ custom-addons/om_hospital/models/appointment.py
@@ -7,7 +7,8 @@ class HospitalAppointment(models.Model):
     _description = "Hospital Appointment"
     _rec_name = 'ref'
     
-    patient_id = fields.Many2one(comodel_name="hospital.patient", string="Appointment")
+    # patient_id = fields.Many2one(comodel_name="hospital.patient", string="Appointment", ondelete="restrict")
+    patient_id = fields.Many2one(comodel_name="hospital.patient", string="Appointment", ondelete="cascade")
     appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
     booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
     # gender = fields.Selection(related="patient_id.gender", readonly=False)
