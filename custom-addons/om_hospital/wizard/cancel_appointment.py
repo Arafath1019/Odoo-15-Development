8c466a8c95b804d402d8e3a5e7182fd833b971c0
yeasin.arafath <yeasin.arafath@bjitgroup.com>
2024-02-15 16:01:06 +0600

    feat: access value from settings

--- custom-addons/om_hospital/wizard/cancel_appointment.py
+++ custom-addons/om_hospital/wizard/cancel_appointment.py
@@ -1,6 +1,8 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 import datetime
+from datetime import date
+from dateutil import relativedelta
 
 class CancelAppointmentWizard(models.TransientModel):
     _name = 'cancel.appointment.wizard'
@@ -20,7 +22,10 @@ class CancelAppointmentWizard(models.TransientModel):
     date_cancel = fields.Date(string="Cancellation Date")
     
     def action_cancel(self):
-        if self.appointment_id.booking_date == fields.Date.today():
-            raise ValidationError(_("Sorry, cancellation is not allowed on the same day of booking!"))
+        cancel_day = self.env['ir.config_parameter'].get_param('om_hospital.cancel_day')
+        allowed_date = self.appointment_id.booking_date - relativedelta.relativedelta(days=int(cancel_day))
+        if allowed_date < date.today:
+            raise ValidationError("Sorry, cancellation is not allowed for this booking!")
+        # if self.appointment_id.booking_date == fields.Date.today():
+        #     raise ValidationError(_("Sorry, cancellation is not allowed on the same day of booking!"))
         self.appointment_id.state = 'cancel'
-        return
